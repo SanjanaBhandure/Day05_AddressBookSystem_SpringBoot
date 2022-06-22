887d0626953da63e4db4f713fbd086362552f1fe
SanjanaBhandure <35860614+SanjanaBhandure@users.noreply.github.com>
2022-06-22 10:02:40 +0530

    Added: Address Book System Project using SpringBoot

---
+++ SpringAddressBookAppDevelopment/src/main/java/com/bridgelabz/addressbookapp/dto/AddressbookDTO.java
@@ -0,0 +1,45 @@
+package com.bridgelabz.addressbookapp.dto;
+
+import lombok.Data;
+import lombok.ToString;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+@Data
+@ToString
+public class AddressbookDTO {
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "FirstName name is Invalid")
+    @NotEmpty(message = "FirstName  cannot be null")
+    public String firstName;
+
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "LastName name is Invalid")
+    @NotEmpty(message = "LastName name cannot be null")
+    public String lastName;
+
+    @NotEmpty(message = "Address should not be empty")
+    public String address;
+
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "City name is Invalid")
+    @NotEmpty(message = "City name  cannot be null")
+    public String city;
+
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "state name is Invalid")
+    @NotEmpty(message = "State name  cannot be null")
+    public String state;
+
+    @Pattern(regexp = "^[0-9]{3}[\\\\s]*[0-9]{2,}$",message = "Zip name is invalid")
+    @NotEmpty(message = "Zip code must not be null")
+    public String zip;
+
+    @Pattern(regexp = "^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$",message = "phonenumber is invalid")
+    @NotEmpty(message = "phone number is not empty")
+    public String phone;
+
+    @Pattern(regexp = "^[a-z0-9]+@[a-z]+\\.[a-z]{2,3}$", message = "email is invalid")
+    @NotEmpty(message = "email should not be empty")
+    public String email;
+
+
+
+}
---
+++ SpringAddressBookAppDevelopment/src/main/java/com/bridgelabz/addressbookapp/model/AddressbookData.java
@@ -0,0 +1,45 @@
+package com.bridgelabz.addressbookapp.model;
+
+import com.bridgelabz.addressbookapp.dto.AddressbookDTO;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="contact_table")
+public class AddressbookData {
+    @Id
+    @Column(name = "person_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int personId;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phone;
+    private String email;
+
+
+    public AddressbookData( AddressbookDTO addressbookDTO) {
+        this.updateAddressBookdata(addressbookDTO);
+
+    }
+
+    public AddressbookData() {
+
+    }
+
+    public void updateAddressBookdata(AddressbookDTO addressbookDTO) {
+        this.firstName = addressbookDTO.firstName;
+        this.lastName = addressbookDTO.lastName;
+        this.address = addressbookDTO.address;
+        this.city = addressbookDTO.city;
+        this.state = addressbookDTO.state;
+        this.zip = addressbookDTO.zip;
+        this.phone = addressbookDTO.phone;
+        this.email = addressbookDTO.email;
+    }
+}
